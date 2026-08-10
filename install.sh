7808e9434ae1c4b05faf21d0bf41cbca6c486b59
Qcby <35736643+Qcby@users.noreply.github.com>
2026-08-10 19:33:42 +0800

    2

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@
 #   bash install.sh status|address|logs|restart|reset-password|help
 #
 # 管道：
-#   curl -fsSL https://raw.githubusercontent.com/Qcby/QcbyScript/code/install.sh | bash                         # 打开交互菜单
-#   curl -fsSL https://raw.githubusercontent.com/Qcby/QcbyScript/code/install.sh | bash -s -- install 8110 latest # 明确指定安装
-#   curl -fsSL https://raw.githubusercontent.com/Qcby/QcbyScript/code/install.sh | bash -s -- update 8110 1.0.4
+#   curl -fsSL https://cdn.jsdelivr.net/gh/Qcby/QcbyScript/code/install.sh | bash                         # 打开交互菜单
+#   curl -fsSL https://cdn.jsdelivr.net/gh/Qcby/QcbyScript/code/install.sh | bash -s -- install 8110 latest # 明确指定安装
+#   curl -fsSL https://cdn.jsdelivr.net/gh/Qcby/QcbyScript/code/install.sh | bash -s -- update 8110 1.0.4
 #
 # 环境变量：
 #   IMAGE_TAG=latest         指定镜像版本
@@ -39,7 +39,7 @@ DEFAULT_HOST_PORT="8110"
 DEFAULT_IMAGE_TAG="${IMAGE_TAG:-latest}"
 MIRROR_URL="${MIRROR_URL:-https://docker.1ms.run}"
 PROJECT_URL="https://hub.docker.com/r/qcby/qcby-vxcode"
-SCRIPT_URL="https://raw.githubusercontent.com/Qcby/QcbyScript/code/install.sh"
+SCRIPT_URL="https://cdn.jsdelivr.net/gh/Qcby/QcbyScript/code/install.sh"
 ADMIN_AUTH_FILE="/app/data/admin_auth.json"
 APP_PLATFORM="${APP_PLATFORM:-auto}"
 # ==================================================
